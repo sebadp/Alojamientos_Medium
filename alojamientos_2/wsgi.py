b08f6ee682abb53104ff32536910b323db805237
seba <sebastian.davila.personal@gmail.com>
2021-03-06 07:36:08 -0300

    heroku deploy done

--- alojamientos_2/wsgi.py
+++ alojamientos_2/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alojamientos_2.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alojamientos_2.settings.production')
 
 # application = get_wsgi_application()
 
