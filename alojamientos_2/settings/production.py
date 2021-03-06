57860ee167c10bf260acb5fd844df1da2ddb09e5
seba <sebastian.davila.personal@gmail.com>
2021-03-06 06:59:30 -0300

    heroku deploy

--- alojamientos_2/settings/production.py
+++ alojamientos_2/settings/production.py
@@ -17,6 +17,8 @@ DATABASES = {
         'USER':'pdjdsilreeaovy',
         'PASSWORD': '9d8558e9c87f19c845257443f3202d86861c0bc039360138f22d6d0d58f5e157',
         'HOST': 'ec2-54-145-249-177.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'PORT': 5432,
     }
 }
+
+STATICFILES_DIR = (BASE_DIR, 'static')
